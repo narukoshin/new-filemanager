3362d728f1a89fc8114dc273279f34e42d01f9d7
Naru K <k@narukoshin.me>
2026-08-14 23:17:58 +0300

    feat: frontend security hardening

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,43 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    build: {
+        sourcemap: false,
+        minify: "terser",
+        terserOptions: {
+            compress: {
+                defaults: true,
+                passes: 2,
+                drop_debugger: true,
+                drop_console: true,
+                dead_code: true,
+                unused: true,
+                side_effects: true,
+                conditionals: true,
+                comparisons: true,
+                evaluate: true,
+                booleans: true,
+                sequences: true,
+                join_vars: true,
+                collapse_vars: true,
+                reduce_vars: true,
+                reduce_funcs: true,
+                hoist_funs: true,
+                inline: true,
+                switches: true
+            },
+            mangle: {
+                properties: false,
+                toplevel: true
+            },
+            keep_fnames: false,
+            keep_classnames: false,
+            format: {
+                comments: false,
+                beautify: false
+            },
+        },
+    }
 })
