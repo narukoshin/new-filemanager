77dc971c200d67430b491483f7b14f10ffcdd75d
Naru K <k@narukoshin.me>
2026-08-18 00:10:55 +0300

    docs: godoc comments

--- backend/internal/users/validate.go
+++ backend/internal/users/validate.go
@@ -4,8 +4,10 @@ import (
 	"regexp"
 )
 
+// usernameRegex is the regular expression used to validate usernames.
 var usernameRegex = regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
 
+// validateUsername validates the username.
 func validateUsername(username string) error {
 	if len(username) < 3 {
 		return ErrUsernameTooShort
